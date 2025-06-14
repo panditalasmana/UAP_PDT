17c6b9208c3344ddbeff4ba117a82676fcef6a23
fajriyah12 <151291668+fajriyah12@users.noreply.github.com>
2025-06-14 12:11:45 +0700

    Add files via upload

---
+++ includes_/footer.php
@@ -0,0 +1,27 @@
+<footer class="bg-dark text-light py-4 mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h5><i class="bi bi-motorcycle"></i> NARIPA WHEELS</h5>
+                    <p>Sistem manajemen penyewaan motor terpercaya</p>
+                </div>
+                <div class="col-md-6 text-md-end">
+                    <p>&copy; 2024 NARIPA WHEELS. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        // Auto-hide alerts after 5 seconds
+        setTimeout(function() {
+            const alerts = document.querySelectorAll('.alert');
+            alerts.forEach(function(alert) {
+                const bsAlert = new bootstrap.Alert(alert);
+                bsAlert.close();
+            });
+        }, 5000);
+    </script>
+</body>
+</html>
